a18229a00a183f0f1c174a22262a735d9688b765
student <student@spbacu.ru>
2026-02-19 14:46:46 +0300

    работа с базой данных

--- backend/data/database.py
+++ backend/data/database.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import select
+from models import User, Valentine
 
 DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
@@ -13,4 +15,27 @@ async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
 
+# --- CRUD функции ---
 
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(User).where(User.email == email))
+    return result.scalar_one_or_none()
+
+async def create_user(db: AsyncSession, name: str, email: str, password: str):
+    user = User(name=name, email=email, password=password)
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+async def get_all_users(db: AsyncSession):
+    result = await db.execute(select(User))
+    return result.scalars().all()
+
+async def delete_user(db: AsyncSession, user_id: int):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+    if user:
+        await db.delete(user)
+        await db.commit()
+    return user
